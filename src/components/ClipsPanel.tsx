fc5a00269549b349cc24e60b3607402e19debd59
Jacob Hillman <jacobhillman@Mac-601.lan>
2026-04-02 11:45:57 -0400

    [7.2] Prevent clips panel overflow on narrow desktop widths

--- src/components/ClipsPanel.tsx
+++ src/components/ClipsPanel.tsx
@@ -21,7 +21,7 @@ export function ClipsPanel({ clips, activeClipId, playingClipId, dispatch, onPla
   }
 
   return (
-    <div className="flex-1 overflow-y-auto p-4 space-y-2">
+    <div className="flex-1 overflow-y-auto overflow-x-hidden p-4 space-y-2 min-w-0">
       {clips.map((clip, i) => (
         <ClipCard
           key={clip.id}
